c95faf7456918b167ec0bc855a0dd93a7fe09b86
Norman Cohen Sabban <normancs@hotmail.fr>
2017-08-29 16:37:15 +0200

    Add figaro - Protect my secret data in config/application.yml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
-class ApplicationController < ActionController::Base
+    class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_doctor!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :adress, :city, :zip_code, :country, :phone_number, :description, :specialty, :RPPS])
+
+    # For additional in app/views/devise/registrations/edit.html.erb
+    devise_parameter_sanitizer.permit(:account_update, keys: [:address, :zip_code,
+      :city, :country, :description, :phone_number])
+  end
+
 end
--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,10 +1,19 @@
 class DoctorsController < ApplicationController
-  def dashboard
-  end
 
-  def history
-  end
+def dashboard
+end
 
-  def answer
-  end
+def history
+end
+
+def answer
+end
+
+private
+
+def doctor_params
+  params.require(:doctor).permit(:address, :zip_code,
+    :city, :country, :description, :phone_number)
+end
+
 end

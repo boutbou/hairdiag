d53940133d51087b00cd24771abdfc19acf02fdb
Timothee Reynaud <reynaudtimothee@gmail.com>
2017-08-31 11:39:47 +0200

    added database info dans les lib

---
+++ lib/assets/db-info.rb
@@ -0,0 +1,19 @@
+
+
+# Table PATIENTS - Colonne STATUS :
+
+# - started : le patient a commencé le questionnaire
+# - ended : le patient a submit le questionnaire
+# - payment-failed : le patient a essayé de payer mais paiement non validé
+# - payment-success : le patient a payé la prestation
+# - assigned : un docteur est lié au patient
+# - answered : le docteur a répondu au patient
+# - opened : le patient a reçu et ouvert l'email du docteur
+
+
+# Table CASES-ASSIGMENTS / Colonnes : ALL
+
+# Case assignments : Definition de la table case assignment : assignation temporaire,
+# on met le patient dans un pool de médecin, tant que le patient n'a pas été
+# attribué définitivement à un médecin
+

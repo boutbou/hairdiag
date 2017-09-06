5e0d4d7d26af6718b11de7fa3a2416a590f0d714
Colin Darie <colin@darie.eu>
2017-09-06 13:02:24 +0200

    Evite que le form basique s’affiche brièvement au chargement de la page et à la validation du form (laoder)

--- app/assets/javascripts/case_form.js
+++ app/assets/javascripts/case_form.js
@@ -3,6 +3,14 @@ $(document).ready(function() {
     formEl: document.getElementById("patient-form"),
     context: document.getElementById("col-form"),
     userImage: "https://static1.disfrazzes.com/productos/miniaturas3/foto-661RU.jpg",
-    robotImage: "https://images.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.TYmMcjbcOJE_2Dwi2acjcgELEs%26pid%3D15.1&f=1"
+    robotImage: "https://images.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.TYmMcjbcOJE_2Dwi2acjcgELEs%26pid%3D15.1&f=1",
+    submitCallback: function() {
+      // on gère nous même la soumission du form
+      // pour éviter de voir le form le temps de la soumission
+      // Affiche un loader à la place.
+      $("#patient-form").submit().hide();
+      $("#form-loading").show();
+      window.ConversationalForm.remove();
+    }
   });
 });
